3ad54a9865e1e53df9359bd01a80e5b4de438939
Jose Garzon <jd.garzon12@gmail.com>
2020-10-26 11:39:54 -0500

    change dll publicpath

--- webpack.config.js
+++ webpack.config.js
@@ -51,6 +51,7 @@ module.exports = {
     new AddAssetHtmlWebpackPlugin({
       filepath: path.resolve(__dirname, 'dist/js/*.dll.js'),
       outputPath: 'js',
+      publicPath: 'https://cozy-place.vercel.app/js',
     }),
   ],
   optimization: {
