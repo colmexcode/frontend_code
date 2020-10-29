318d523ca43b793e2e2b975e04a593adb0f765e2
Sergio Ruiz <shecho27@gmail.com>
2020-10-29 00:39:48 -0500

    Fix state categories

--- src/pages/InterestForm/index.js
+++ src/pages/InterestForm/index.js
@@ -1,5 +1,5 @@
 // ------------------------------ import libraries
-import React from 'react';
+import React, { useState } from 'react';
 
 // ------------------------------ import components
 import { Button } from '../../global-styles/Buttons';
@@ -19,24 +19,51 @@ import {
 // ------------------------------------ COMPONENT ------------------------------------//
 // description of the component.
 export default () => {
+  const [categories, setCategories] = useState([]);
   return (
     <Container>
       <Title>WHAT ARE YOU UP TO?</Title>
       <DescriptText>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-        Aliquid, veritatis maxime sed blanditiis ab, similique magnam
-        aperiam vero eius et dolores quidem velit voluptas cupiditate.
+        For decades travellers have reached for Lonely Planet books
+        when looking to plan and execute their perfect trip, but now,
+        they can also let Lonely Planet Experiences lead the way.
       </DescriptText>
       <CategorySection>
-        <Button>Adventure Travel</Button>
-        <Button>Art And Culture</Button>
-        <Button>Wildlife And Nature</Button>
-        <Button>Family Holidays</Button>
-        <Button>Food And Drink</Button>
+        <Button
+          name="Adventure Travel"
+          onClick={(e) => setCategories(categories + e.target.name)}
+        >
+          Adventure Travel
+        </Button>
+        <Button
+          name="Art And Culture"
+          onClick={(e) => setCategories(categories + e.target.name)}
+        >
+          Art And Culture
+        </Button>
+        <Button
+          name="Wildlife And Nature"
+          onClick={(e) => setCategories(categories + e.target.name)}
+        >
+          Wildlife And Nature
+        </Button>
+        <Button
+          name="Family Holidays"
+          onClick={(e) => setCategories(categories + e.target.name)}
+        >
+          Family Holidays
+        </Button>
+        <Button
+          name="Food And Drink"
+          onClick={(e) => setCategories(categories + e.target.name)}
+        >
+          Food And Drink
+        </Button>
       </CategorySection>
       <SelectionSection>
         <TextChoice>
-          Awesome Choice: category1, category2, category3
+          Awesome Choice:
+          {categories}
         </TextChoice>
         <Button main>GO</Button>
       </SelectionSection>
